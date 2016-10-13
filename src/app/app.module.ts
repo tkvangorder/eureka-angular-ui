6d373d84a2ecb2cc7b9714a2500803b47941fa01
Tyler Van Gorder <tyler.vangoder@build.com>
2016-10-13 11:16:00 -0700

    Checking in bootstrap styling changes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AlertModule} from 'ng2-bootstrap/ng2-bootstrap';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
@@ -18,7 +18,8 @@ import { SearchResultsComponent } from './search-results/search-results.componen
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertModule} from 'ng2-bootstrap/ng2-bootstrap';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  styleUrls: ['./search-bar.component.scss'],
+  
 })
 export class SearchBarComponent implements OnInit {
 
+  public alerts:Array<Object> = [
+    {
+      type: 'info',
+      message: 'Enter Criteria to find matching services.'
+    }
+  ];
+
+  public closeAlert(i:number):void {
+    this.alerts.splice(i, 1);
+  }
+
+  public addAlert(message: String, type: String):void {
+    this.alerts.push({message: message, type: type, closable: true});
+  }
   constructor() { }
 
   ngOnInit() {
